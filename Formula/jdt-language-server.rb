19f0b746648ae4e3f359192b8d50efe2d86c04df
liyong <hungrybirder@gmail.com>
2021-01-04 17:04:47 +0800

    Add jdt-language-server.rb

---
+++ Formula/jdt-language-server.rb
@@ -0,0 +1,22 @@
+class JdtLanguageServer < Formula
+  desc "Eclipse JDT Language Server"
+  homepage "https://github.com/eclipse/eclipse.jdt.ls"
+  url "https://download.eclipse.org/jdtls/snapshots/jdt-language-server-0.68.0-202012242230.tar.gz"
+  sha256 "f6a81d50e459effde0ff1d82683a8039dde282daa40cc8844d4dd7498fa48290"
+
+  depends_on "openjdk"
+
+  def install
+    rm_rf "config_linux"
+    rm_rf "config_win"
+    libexec.install ["config_mac", "features", "plugins"]
+    (bin/"jdt-language-server").write <<~EOS
+      #!/bin/bash
+      JDT_LS_HOME="#{libexec}"
+      JDT_LS_LAUNCHER=$(find $JDT_LS_HOME -name "org.eclipse.equinox.launcher_*.jar")
+      JDT_LS_HEAP_SIZE=${JDT_LS_HEAP_SIZE:=-Xmx12G}
+      JAVA_BIN="/usr/local/opt/openjdk/bin/java"
+      exec ${JAVA_BIN} -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -noverify $JDT_LS_HEAP_SIZE -jar "$JDT_LS_LAUNCHER" -configuration "$JDT_LS_HOME/config_mac" --add-modules=ALL-SYSTEM --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED -data "$HOME/workspace"
+    EOS
+  end
+end
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# homebrew-repo
+# Hungrybirder Repo
+
+## How do I install these formulae?
+
+```bash
+brew install hungrybirder/repo/jdt-language-server
+```
+
+Or
+
+```bash
+brew tap hungrybirder/repo
+brew install jdt-jdt-language-server
+```
