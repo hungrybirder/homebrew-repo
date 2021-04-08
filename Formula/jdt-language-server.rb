9fc38082f7d9d50066196992f0757ee7ea19a14a
liyong <hungrybirder@gmail.com>
2021-04-08 10:02:20 +0800

    jdt-language-server-0.71.0-202104060951

--- Formula/jdt-language-server.rb
+++ Formula/jdt-language-server.rb
@@ -1,10 +1,8 @@
 class JdtLanguageServer < Formula
   desc "Eclipse JDT Language Server"
   homepage "https://github.com/eclipse/eclipse.jdt.ls"
-  url "https://download.eclipse.org/jdtls/snapshots/jdt-language-server-0.71.0-202103240306.tar.gz"
-  sha256 "386e412cbee1dfd8b6219e50e97c44809ca6fe51b0569a498180e10245cd05b1"
-  # url "https://download.eclipse.org/jdtls/snapshots/jdt-language-server-0.69.0-202102120513.tar.gz"
-  # sha256 "4ced0b7a2e5087df5c7e74b4124d62b38f7edfc627f6c20c3c0254aeb7004113"
+  url "https://download.eclipse.org/jdtls/snapshots/jdt-language-server-0.71.0-202104060951.tar.gz"
+  sha256 "c8d64340aeb182ad230101753497272402e304b5e4401e0b330592df347c803f"
 
   depends_on "openjdk"
 
