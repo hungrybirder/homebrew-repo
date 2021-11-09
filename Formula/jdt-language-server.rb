ee6dc24e9f27e1accefb88066ce9576a4fbf9163
liyong <hungrybirder@gmail.com>
2021-11-09 13:12:35 +0800

    Add java-lsp.

--- Formula/jdt-language-server.rb
+++ Formula/jdt-language-server.rb
@@ -18,5 +18,27 @@ class JdtLanguageServer < Formula
       JAVA_BIN="$(brew --prefix)/opt/openjdk/bin/java"
       exec ${JAVA_BIN} -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -noverify $JDT_LS_HEAP_SIZE -jar "$JDT_LS_LAUNCHER" -configuration "$JDT_LS_HOME/config_mac" --add-modules=ALL-SYSTEM --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED -data "$HOME/workspace"
     EOS
+    (bin/"java-lsp").write <<~EOS
+      #!/bin/bash
+      JDT_LS_HOME="#{libexec}"
+      JDT_LS_LAUNCHER=$(find $JDT_LS_HOME -name "org.eclipse.equinox.launcher_*.jar")
+      JDT_LS_HEAP_SIZE=${JDT_LS_HEAP_SIZE:=-Xmx12G}
+      JAVA_BIN="java"
+      GRADLE_HOME="$(brew --prefix)/opt/gradle"
+      export GRADLE_HOME
+      exec $JAVA_BIN \
+        -Declipse.application=org.eclipse.jdt.ls.core.id1 \
+        -Dosgi.bundles.defaultStartLevel=4 \
+        -Declipse.product=org.eclipse.jdt.ls.core.product \
+        -Dlog.protocol=true \
+        -Dlog.level=ALL \
+        $JDT_LS_HEAP_SIZE \
+        -jar "$JDT_LS_LAUNCHER" \
+        -configuration "$JDT_LS_HOME/config_mac" \
+        -data "$1" \
+        --add-modules=ALL-SYSTEM \
+        --add-opens java.base/java.util=ALL-UNNAMED \
+        --add-opens java.base/java.lang=ALL-UNNAMED
+    EOS
   end
 end
